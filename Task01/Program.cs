515d35d8c4dc227a59db52cc7a76609f6b7ff027
Anna <kochurovaav@gmail.com>
2023-02-24 15:21:57 +0200

    Task01 is done

---
+++ Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Алгоритм обхода директории
+
+void CatalogInfo (string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"C:\Users\User\HW2_CSharp"   ;
+CatalogInfo(path);
